f3edef7c9ae03b0583d1af944823df1111e71cdd
a-y-a-n-das <ayandas8125@gmail.com>
2025-10-05 11:28:19 +0530

    Clear-Button-Fix

--- logic.js
+++ logic.js
@@ -18,6 +18,7 @@ const lname= document.getElementById("lname").value;
 const course= document.getElementById("course").value;
 const year = document.getElementsByName("year");
 const pdf= document.getElementById('pdf');
+const clear = document.getElementById('clear');
 
 let selectedYear = '';
     for (let i = 0; i < year.length; i++) {
@@ -35,10 +36,17 @@ document.getElementById("cert-year").textContent= `${selectedYear}`;
 document.getElementById("certificate").style.display= 'block';
 document.getElementById('pdf').style.display='block';
 
+
+
+
+console.log(`${fname} ${lname} ${course}`); 
+});
+clear.addEventListener('click', ()=>{
+  form.reset();
+  form.querySelector('input').focus();
+})
 pdf.addEventListener('click', ()=>{
   html2pdf().from(certificate).save("certificate.pdf")
 });
 
-console.log(`${fname} ${lname} ${course}`); 
-});
 });
